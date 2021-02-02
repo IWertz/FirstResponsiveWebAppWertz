3cd635fbf1e9a781ccbea57d0a42bda077524429
IWertz <77807438+IWertz@users.noreply.github.com>
2021-02-01 19:23:08 -0600

    added content

---
+++ FirstResponsiveWebAppWertz/FirstResponsiveWebAppWertz/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppWertz.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstResponsiveWebAppWertz.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Age = 0;
+            ViewBag.Name = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ResponsiveWebModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Age = model.AgeThisYear();
+                ViewBag.Name = model.Name;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppWertz/FirstResponsiveWebAppWertz/Models/ResponsiveWebModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstResponsiveWebAppWertz.Models
+{
+    public class ResponsiveWebModel
+    {
+        public const int THIS_YEAR = 2021;
+           [Required]
+           public string Name { get; set; }
+           [Required]
+           [Range(1900, 2021)]
+           public int? BirthYear { get; set; }
+
+           public int AgeThisYear()
+        {
+            return THIS_YEAR - BirthYear.Value;
+        }
+    }
+}
